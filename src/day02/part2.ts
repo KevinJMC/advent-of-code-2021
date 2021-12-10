4047a65176601f3a2071dc5f8d1ab800bc7b6039
Kevin McCann <25021058+KevinJMC@users.noreply.github.com>
2021-12-10 12:27:35 -0500

    Day 2, Part 2

--- src/day02/part2.ts
+++ src/day02/part2.ts
@@ -1,5 +1,22 @@
 // Advent of Code - Day 2 - Part Two
 
 export function part2(input: string): number {
-  return 0;
+  const inputArray = input.split('\n');
+  let [horizontalPos, vertPos] = [0, 0];
+  let aim = 0;
+  for(let index = 0; index < inputArray.length; index++) {
+    const val = inputArray[index].split(' ');
+    const [direction, distance] = val;
+    if(direction.startsWith('f')) {
+      horizontalPos += Number(distance);
+      vertPos += aim * Number(distance);
+    }
+    if(direction.startsWith('b')) {
+      horizontalPos -= Number(distance);
+      vertPos -= aim * Number(distance);
+    }
+    if(direction.startsWith('d')) aim += Number(distance)
+    if(direction.startsWith('u')) aim -= Number(distance)
+  }
+  return horizontalPos * vertPos
 }
